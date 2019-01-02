822dd92f04e76ced34b03330b7b41f3cf3f2a69a
evenhumble <patrickwuke@163.com>
2019-01-02 13:18:31 +0800

    add MYSQL DOCKER Script and .gitignore file

--- dh-tester-ansible/roles/hbase/files/start_local_hbase.sh
+++ dh-tester-ansible/roles/hbase/files/start_local_hbase.sh
@@ -4,7 +4,7 @@
 # 2. copy config file allround server
 # 3. start hbase 
 # 4. running hbase and hadoop in a same server
-HBASE_HOME=hbase-2.1.0
+HBASE_HOME=hbase-2.1.1
 echo "copy hbase site files to config"
 cp -f hbase-site_local.xml ${HBASE_HOME}/conf/hbase-site.xml
 echo "start hbase"
